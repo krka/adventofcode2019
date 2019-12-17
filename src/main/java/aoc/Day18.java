34305a0477903e7ad95466b843c8b5acc015b6ea
Kristofer Karlsson <kristofer.karlsson@gmail.com>
2019-12-17 18:09:08 +0100

    Prepare for Day 18

---
+++ src/main/java/aoc/Day18.java
@@ -0,0 +1,19 @@
+package aoc;
+
+import util.Util;
+
+import java.util.List;
+
+public class Day18 {
+  public Day18(String name) {
+    List<String> input = Util.readResource(name);
+  }
+
+  public int part1() {
+    return 0;
+  }
+
+  public int part2() {
+    return 0;
+  }
+}
---
+++ src/test/java/aoc/Day18Test.java
@@ -0,0 +1,18 @@
+package aoc;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Day18Test {
+
+  @Test
+  public void testPart1() {
+    assertEquals(0, new Day18("day18.in").part1());
+  }
+
+  @Test
+  public void testPart2() {
+    assertEquals(0, new Day18("day18.in").part2());
+  }
+}
