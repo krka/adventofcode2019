dfd16c2ea6305e085c26c4bba52281f6d77ecb27
Kristofer Karlsson <krka@spotify.com>
2020-12-02 18:07:06 +0100

    Implement Day2 in IntCode

---
+++ src/main/resources/2020/day2.asm
@@ -0,0 +1,39 @@
+int x = 'a'
+
+#include outputs.asm
+#include stdlib.asm
+int num_entries = read_int()
+
+int count = 0
+array[2] c
+array[1000] pw
+
+for case = 0, case < num_entries do
+
+  skip_non_int()
+  int min = read_int()
+
+  skip_non_int()
+  int max = read_int()
+
+  skip_non_chars()
+  read_chars(c)
+
+  skip_non_chars()
+  read_chars(pw)
+
+  int c0 = c[0]
+  int matches = 0
+  int len = strlen(pw)
+  for i = 0, i < len do
+    if pw[i] == c0 then
+      matches = matches + 1
+    end
+  end
+  if matches >= min && matches <= max then
+    count = count + 1
+  end
+end
+
+outputNumber(count)
+output(10)
--- src/test/java/aoc2020/Day2Test.java
+++ src/test/java/aoc2020/Day2Test.java
@@ -1,6 +1,12 @@
 package aoc2020;
 
+import intcode.IntCode;
+import intcode.assembler.AnnotatedIntCode;
+import intcode.assembler.Assembler;
 import org.junit.Test;
+import util.Util;
+
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
@@ -8,6 +14,21 @@ public class Day2Test {
   @Test
   public void testPart1() {
     assertEquals(542, new Day2("2020/day2.in").solvePart1());
+  }
+
+  @Test
+  public void testPart1IntCode() {
+    AnnotatedIntCode compiled = Assembler.compileAnnotated("2020/day2.asm");
+    System.out.println(compiled.toString());
+    IntCode intCode = IntCode.fromResource(compiled.getIntCode());
+
+    // Number of lines in input
+    intCode.writeASCIILine("1000");
+    Util.readResource("2020/day2.in").forEach(intCode::writeASCIILine);
+    intCode.run();
+    assertEquals(IntCode.State.HALTED, intCode.getState());
+    List<String> output = intCode.readAllASCIILines();
+    assertEquals(List.of("542"), output);
   }
 
   @Test
