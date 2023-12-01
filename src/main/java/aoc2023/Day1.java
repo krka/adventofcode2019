8143b1d6249067bc87b5c76b2eee85dd09a448b8
Kristofer Karlsson <krka@spotify.com>
2023-12-01 09:49:44 +0100

    Simplify 2023 Day 1

--- src/main/java/aoc2023/Day1.java
+++ src/main/java/aoc2023/Day1.java
@@ -6,10 +6,7 @@ import util.Util;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.regex.Matcher;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
 public class Day1 implements Day {
 
@@ -22,18 +19,15 @@ public class Day1 implements Day {
     }
   }
 
+  private static final String SPELLED = "one two three four five six seven eight nine";
+
   static final Map<String, Integer> DIGITS_SPELLED = new HashMap<>();
   static {
     DIGITS_SPELLED.putAll(DIGITS);
-    DIGITS_SPELLED.put("one", 1);
-    DIGITS_SPELLED.put("two", 2);
-    DIGITS_SPELLED.put("three", 3);
-    DIGITS_SPELLED.put("four", 4);
-    DIGITS_SPELLED.put("five", 5);
-    DIGITS_SPELLED.put("six", 6);
-    DIGITS_SPELLED.put("seven", 7);
-    DIGITS_SPELLED.put("eight", 8);
-    DIGITS_SPELLED.put("nine", 9);
+    int i = 0;
+    for (String s : SPELLED.split(" ")) {
+      DIGITS_SPELLED.put(s, ++i);
+    }
   }
 
   public Day1(String name) {
@@ -57,62 +51,24 @@ public class Day1 implements Day {
   }
 
   private long digits(String x, Map<String, Integer> map) {
-    final int[] array = IntStream.range(0, x.length())
-            .map(start -> {
-              final char c = x.charAt(start);
-              if (c >= '0' && c <= '9') {
-                return c - '0';
-              }
-              switch (c) {
-                case 'o':
-                  return ifMatch(x, start, "one", map);
-                case 't': {
-                  if (x.length() - start >= 2) {
-                    switch (x.charAt(start + 1)) {
-                      case 'w': return ifMatch(x, start, "two", map);
-                      case 'h': return ifMatch(x, start, "three", map);
-                    }
-                  }
-                  return -1;
-                }
-                case 'f': {
-                  if (x.length() - start >= 2) {
-                    switch (x.charAt(start + 1)) {
-                      case 'o': return ifMatch(x, start, "four", map);
-                      case 'i': return ifMatch(x, start, "five", map);
-                    }
-                  }
-                  return -1;
-                }
-                case 's': {
-                  if (x.length() - start >= 2) {
-                    switch (x.charAt(start + 1)) {
-                      case 'i': return ifMatch(x, start, "six", map);
-                      case 'e': return ifMatch(x, start, "seven", map);
-                    }
-                  }
-                  return -1;
-                }
-                case 'e': return ifMatch(x, start, "eight", map);
-                case 'n': return ifMatch(x, start, "nine", map);
-                default: return -1;
-              }
-            }).filter(v -> v != -1)
-            .toArray();
-
-    return array[0] * 10 + array[array.length - 1];
+    int first = find23(x, map, 0, 1);
+    int last = find23(x, map, x.length() - 1, -1);
+    return first * 10 + last;
   }
 
-  private int ifMatch(String s, int start, String key, Map<String, Integer> map) {
-    final int remaining = s.length() - start;
-    final int len = key.length();
-    if (remaining < len) {
-      return -1;
+  private int find23(String x, Map<String, Integer> map, int start, int step) {
+    while (start >= 0 && start < x.length()) {
+      for (Map.Entry<String, Integer> entry : map.entrySet()) {
+        final String key = entry.getKey();
+        if (start + key.length() <= x.length()) {
+          if (x.subSequence(start, start + key.length()).equals(key)) {
+            return entry.getValue();
+          }
+        }
+      }
+      start += step;
     }
-    if (s.subSequence(start, start + len).equals(key)) {
-      return map.getOrDefault(key, -1);
-    }
-    return -1;
+    throw new RuntimeException();
   }
 
 }
