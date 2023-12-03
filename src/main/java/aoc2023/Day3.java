18bfac5e04ee0e7f0bfafdee5398f311a87f74a9
Kristofer Karlsson <krka@spotify.com>
2023-12-03 12:11:27 +0100

    2023 Day 3 simplified

--- src/main/java/aoc2023/Day3.java
+++ src/main/java/aoc2023/Day3.java
@@ -5,65 +5,53 @@ import util.Grid;
 import util.Util;
 import util.Vec2;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.function.Function;
-import java.util.stream.Collectors;
 
 import static java.lang.Character.isDigit;
 
 public class Day3 implements Day {
 
   private final Grid<Character> grid;
+  private final Grid<AtomicInteger> numbers;
 
   public Day3(String name) {
     grid = Grid.from(Util.readResource(name), '.', Function.identity());
+    numbers = Grid.create(grid.rows(), grid.cols(), null);
+    grid.forEach((row, col, value) -> {
+      if (isDigit(value) && !isDigit(grid.get(row, col - 1))) {
+        StringBuilder sb = new StringBuilder();
+        int c = col;
+        while (isDigit(value)) {
+          sb.append(value);
+          value = grid.get(row, ++c);
+        }
+        AtomicInteger part = new AtomicInteger(Integer.parseInt(sb.toString()));
+        for (int i = col; i < c; i++) {
+          numbers.set(row, i, part);
+        }
+      }
+    });
   }
 
   @Override
   public long solvePart1() {
-    AtomicLong sum = new AtomicLong();
+    Set<AtomicInteger> validParts = new HashSet<>();
     grid.forEach((row, col, value) -> {
-      if (isDigit(value) && !isDigit(grid.get(row, col - 1))) {
-        final String digits = digits(row, col);
-        AtomicBoolean isPart = new AtomicBoolean(false);
-        for (int i = 0; i < digits.length(); i++) {
-          int finalI = i;
-          Vec2.DIRS_8.forEach(vec2 -> {
-            final long r = vec2.row() + row;
-            final long c = vec2.col() + col + finalI;
-            final Character ch = grid.get(r, c);
-            if (isPart(ch)) {
-              isPart.set(true);
-            }
-          });
-        }
-        if (isPart.get()) {
-          sum.addAndGet(Integer.parseInt(digits));
-        }
+      if (!isDigit(value) && value != '.') {
+        Vec2.DIRS_8.forEach(dir -> {
+          final AtomicInteger maybePart = numbers.get(row + dir.row(), col + dir.col());
+          if (maybePart != null) {
+            validParts.add(maybePart);
+          }
+        });
       }
     });
-    return sum.get();
-  }
-
-  private static boolean isPart(char c) {
-    return c != '.' && !isDigit(c);
-  }
-
-  private String digits(int row, int col) {
-    StringBuilder sb = new StringBuilder();
-    while (true) {
-      final Character c = grid.get(row, col);
-      if (isDigit(c)) {
-        sb.append(c);
-        col++;
-      } else {
-        return sb.toString();
-      }
-    }
+    return validParts.stream().mapToLong(AtomicInteger::longValue).sum();
   }
 
   @Override
@@ -71,54 +59,17 @@ public class Day3 implements Day {
     AtomicLong sum = new AtomicLong();
     grid.forEach((row, col, value) -> {
       if (value == '*') {
-        List<String> parts = new ArrayList<>();
-        if (isDigit(grid.get(row, col - 1))) {
-          parts.add(findPart(row, col - 1));
-        }
-        if (isDigit(grid.get(row, col + 1))) {
-          parts.add(findPart(row, col + 1));
-        }
-        if (isDigit(grid.get(row - 1, col))) {
-          parts.add(findPart(row - 1, col));
-        } else {
-          if (isDigit(grid.get(row - 1, col - 1))) {
-            parts.add(findPart(row - 1, col - 1));
-          }
-          if (isDigit(grid.get(row - 1, col + 1))) {
-            parts.add(findPart(row - 1, col + 1));
-          }
-        }
-        if (isDigit(grid.get(row + 1, col))) {
-          parts.add(findPart(row + 1, col));
-        } else {
-          if (isDigit(grid.get(row + 1, col - 1))) {
-            parts.add(findPart(row + 1, col - 1));
-          }
-          if (isDigit(grid.get(row + 1, col + 1))) {
-            parts.add(findPart(row + 1, col + 1));
-          }
-        }
-        final List<Long> gears = parts.stream().filter(s -> !s.isEmpty())
-                .map(Long::parseLong)
-                .collect(Collectors.toList());
-        if (gears.size() == 2) {
-          sum.addAndGet(gears.get(0) * gears.get(1));
+        final long[] parts = Vec2.DIRS_8.stream()
+                .map(dir -> numbers.get(row + dir.row(), col + dir.col()))
+                .filter(Objects::nonNull)
+                .distinct()
+                .mapToLong(AtomicInteger::longValue)
+                .toArray();
+        if (parts.length == 2) {
+          sum.addAndGet(parts[0] * parts[1]);
         }
       }
     });
     return sum.get();
   }
-
-  private String findPart(int row, int col) {
-    final StringBuilder sb = new StringBuilder();
-    while (isDigit(grid.get(row, col - 1))) {
-      col--;
-    }
-    while (isDigit(grid.get(row, col))) {
-      sb.append(grid.get(row, col));
-      col++;
-    }
-    return sb.toString();
-  }
-
 }
