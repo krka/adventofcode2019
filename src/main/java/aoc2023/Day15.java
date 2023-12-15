d46fcf8c01533850b6d4a9e77551c7fbe6b151dc
Kristofer Karlsson <krka@spotify.com>
2023-12-15 09:10:19 +0100

    2023 Day 15 cleanup

--- src/main/java/aoc2023/Day15.java
+++ src/main/java/aoc2023/Day15.java
@@ -5,9 +5,11 @@ import util.Util;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Stream;
 
 public class Day15 implements Day {
@@ -36,62 +38,33 @@ public class Day15 implements Day {
 
   @Override
   public long solvePart2() {
-    final String s = lines.get(0).get(0);
-    final String[] parts = s.split(",");
-    Map<Integer, List<Item>> boxes = new HashMap<>();
-    for (String part : parts) {
-      final String[] split = part.split("[=-]");
+    final String line = lines.get(0).get(0);
+    final String[] parts = line.split(",");
+    Map<Integer, Map<String, AtomicInteger>> boxes = new HashMap<>();
+    for (String s : parts) {
+      final String[] split = s.split("[=-]");
       final String label = split[0];
-      final int box = hash((label));
-      if (part.contains("=")) {
-        final List<Item> items = boxes.computeIfAbsent(box, x -> new ArrayList<>());
-        final int focal = Integer.parseInt(split[1]);
-        final Item item = new Item(label, focal);
-        final int i = items.indexOf(item);
-        if (i != -1) {
-          items.get(i).focal = focal;
-        } else {
-          items.add(item);
-        }
-      } else if (part.contains("-")) {
-        boxes.computeIfAbsent(box, x -> new ArrayList<>()).remove(new Item(label, -1));
+      final int box = hash(label);
+      final Map<String, AtomicInteger> list = boxes.computeIfAbsent(box, x -> new LinkedHashMap<>());
+      if (s.contains("=")) {
+        list.computeIfAbsent(label, x -> new AtomicInteger()).set(Integer.parseInt(split[1]));
+      } else if (s.contains("-")) {
+        list.remove(label);
       } else {
         throw new RuntimeException();
       }
     }
     long sum = 0;
-    for (Map.Entry<Integer, List<Item>> e : boxes.entrySet()) {
+    for (Map.Entry<Integer, Map<String, AtomicInteger>> e : boxes.entrySet()) {
       final int a = e.getKey() + 1;
-      final List<Item> list = e.getValue();
-      for (int j = 0; j < list.size(); j++) {
-        final int c = list.get(j).focal;
-        sum += (long) a * (j + 1) * c;
+      final Map<String, AtomicInteger> list = e.getValue();
+      int k = 0;
+      for (AtomicInteger focal : list.values()) {
+        k++;
+        sum += a * k * focal.get();
       }
     }
     return sum;
-  }
-
-  static class Item {
-    String name;
-    int focal;
-
-    public Item(String name, int focal) {
-      this.name = name;
-      this.focal = focal;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-      if (this == o) return true;
-      if (o == null || getClass() != o.getClass()) return false;
-      Item foo = (Item) o;
-      return Objects.equals(name, foo.name);
-    }
-
-    @Override
-    public int hashCode() {
-      return Objects.hash(name);
-    }
   }
 }
 
