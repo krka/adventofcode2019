21b8db859aa88a83c2f0e907b837252ae1a8ca65
Kristofer Karlsson <krka@spotify.com>
2023-12-15 08:52:15 +0100

    2023 Day 15 cleanup

--- src/main/java/aoc2023/Day15.java
+++ src/main/java/aoc2023/Day15.java
@@ -1,15 +1,14 @@
 package aoc2023;
 
 import util.Day;
-import util.Grid;
 import util.Util;
-import util.Vec2;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.util.stream.Stream;
 
 public class Day15 implements Day {
   private final List<List<String>> lines;
@@ -22,15 +21,11 @@ public class Day15 implements Day {
   public long solvePart1() {
     final String s = lines.get(0).get(0);
     final String[] parts = s.split(",");
-    long sum = 0;
-    for (String part : parts) {
-      sum += hash213(part);
-    }
-    return sum;
+    return Stream.of(parts).mapToInt(this::hash).sum();
   }
 
-  private long hash213(String part) {
-    long sum = 0;
+  private int hash(String part) {
+    int sum = 0;
     for (Character c : Util.toList(part)) {
       sum += c;
       sum *= 17;
@@ -43,32 +38,31 @@ public class Day15 implements Day {
   public long solvePart2() {
     final String s = lines.get(0).get(0);
     final String[] parts = s.split(",");
-    Map<Integer, List<Foo>> boxes = new HashMap<>();
+    Map<Integer, List<Item>> boxes = new HashMap<>();
     for (String part : parts) {
-      final String[] parts2 = part.split("[=-]");
-      final String label = parts2[0];
-      final int box = (int) hash213((label));
+      final String[] split = part.split("[=-]");
+      final String label = split[0];
+      final int box = hash((label));
       if (part.contains("=")) {
-        final List<Foo> strings = boxes.computeIfAbsent(box, x -> new ArrayList<>());
-        final int ocal = Integer.parseInt(parts2[1]);
-        final Foo e = new Foo(label, ocal);
-        final int i = strings.indexOf(e);
+        final List<Item> items = boxes.computeIfAbsent(box, x -> new ArrayList<>());
+        final int focal = Integer.parseInt(split[1]);
+        final Item item = new Item(label, focal);
+        final int i = items.indexOf(item);
         if (i != -1) {
-          strings.get(i).focal = ocal;
+          items.get(i).focal = focal;
         } else {
-          strings.add(e);
+          items.add(item);
         }
       } else if (part.contains("-")) {
-        final List<Foo> strings = boxes.computeIfAbsent(box, x -> new ArrayList<>());
-        strings.remove(new Foo(label, -1));
+        boxes.computeIfAbsent(box, x -> new ArrayList<>()).remove(new Item(label, -1));
       } else {
         throw new RuntimeException();
       }
     }
     long sum = 0;
-    for (Map.Entry<Integer, List<Foo>> e : boxes.entrySet()) {
+    for (Map.Entry<Integer, List<Item>> e : boxes.entrySet()) {
       final int a = e.getKey() + 1;
-      final List<Foo> list = e.getValue();
+      final List<Item> list = e.getValue();
       for (int j = 0; j < list.size(); j++) {
         final int c = list.get(j).focal;
         sum += (long) a * (j + 1) * c;
@@ -77,11 +71,11 @@ public class Day15 implements Day {
     return sum;
   }
 
-  static class Foo {
+  static class Item {
     String name;
     int focal;
 
-    public Foo(String name, int focal) {
+    public Item(String name, int focal) {
       this.name = name;
       this.focal = focal;
     }
@@ -90,7 +84,7 @@ public class Day15 implements Day {
     public boolean equals(Object o) {
       if (this == o) return true;
       if (o == null || getClass() != o.getClass()) return false;
-      Foo foo = (Foo) o;
+      Item foo = (Item) o;
       return Objects.equals(name, foo.name);
     }
 
